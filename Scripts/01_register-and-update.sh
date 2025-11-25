85150ac5da0743643036874d325df01c43edd822
jradtke-suse <james.radtke@suse.com>
2025-11-25 12:08:10 -0500

    Making more universal

--- Scripts/01_register-and-update.sh
+++ Scripts/01_register-and-update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+register_node() {
 SUSEConnect -e <reg_email> -r <reg_code>
 SUSEConnect --product sle-module-basesystem/15.7/x86_64
 SUSEConnect --product sle-module-server-applications/15.7/x86_64
 SUSEConnect --product PackageHub/15.7/x86_64
 zypper refresh
+}
 
 # Install git-core
 zypper -n in git-core
